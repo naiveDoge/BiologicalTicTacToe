81eb7fed5ecd7dd8c3251db195dc25595b846970
Shangren Zhou <zhoushangren@yahoo.com>
2023-01-03 18:43:44 +0800

    Now questions can be loaded from external files.

--- src/Game.js
+++ src/Game.js
@@ -1,130 +1,10 @@
 import { INVALID_MOVE } from 'boardgame.io/core';
 import alertify from 'alertifyjs';
 import $ from 'jquery';
+import 'jquery-easy-loading';
 
-var questionSource = [
-  [
-    'What is the advantage of having specialized muscle tissues (ex. musclar and cardiovascular)?\nA. They can be controlled by different parts of the brain\nB. They work in shifts when the other kinds need rest\nC. There is no advantage of having them specialized',
-    'A',
-  ],
-  [
-    'Which of the following lifestyle decisions might improve your cardiovascular health?\nA. To have a diet that is abundant with saturated fats\nB. To have a job that required sedentary lifestyle\nC. To be proactive in physical exercises, especially aerobics',
-    'C',
-  ],
-  [
-    'What is the major function of the circulatory system?\nA. To support your body standing on its own\nB. To bring nutrients to cells and take wastes away\nC. To fight against infections and diseases',
-    'B',
-  ],
-  [
-    'Which of the following parts of the circulatory system has the largest cross-section area?\nA. Arteries\nB. Veins\nC. Capillaries',
-    'C',
-  ],
-  [
-    'Which of the following parts of the circulatory system has the thickest cell wall?\nA. Arteries\nB. Veins\nC. Arterioles',
-    'A',
-  ],
-  [
-    'Which of the following absorptions could happen in capillaries?\nA. Lipids and gases\nB. Glucoes and lipids\nC. Glucose and gases',
-    'C',
-  ],
-  [
-    'What is the left and right side of the heart divided by?\nA. Valves\nB. Septum\nC. Sphincter muscles',
-    'B',
-  ],
-  [
-    'Which chamber of the heart has the thickest wall?\nA. Left atrium\nB. Left ventricle\nC. Right atrium',
-    'B',
-  ],
-  [
-    'Which kind of valves is present between the ventricles and the blood vessels that they are attached to?\nA. Bicuspid valves\nB. Tricuspid valves\nC. Semilunar valves',
-    'C',
-  ],
-  [
-    'Where does the dexoygenated blood enter the heart from the whole body?\nA. Right atrium\nB. Left atrium\nC. Left ventricle',
-    'A',
-  ],
-  [
-    'Where is the destination of blood pumped out of the left ventricle\nA. The whole body\nB. Lungs\nC. Rght atrium',
-    'A',
-  ],
-  [
-    'What is the contraction of heart muscle called?\nA. Diastole\nB. Systole\nC. Cardiac cycle',
-    'B',
-  ],
-  [
-    'What is the primary cause of atherosclerosis?\nA. Excessive amount of platelets in blood\nB. The arteries become too thin \nC. Hardening of arteries by cholesterol plaque deposits',
-    'C',
-  ],
-  [
-    'Which of the following readings from a sphygmomanometer is considered normal?\nA. 120/85\nB. 140/120\nC. 100/70',
-    'A',
-  ],
-  [
-    'Where does the exchange of gases and nutrients for a baby take place?\nA. Umbilical arteries\nB. Placenta\nC. Oval opening',
-    'B',
-  ],
-  [
-    'Which type of structure as a protein is common for hemoglobin in blood?\nA. Primary\nB. Secondary\nC. Quaternary',
-    'C',
-  ],
-  [
-    'Where will hemoglobin pick up oxygen molecules?\nA. Acidic tissues\nB. Neutral tissues\nC. Warmer tissues',
-    'B',
-  ],
-  [
-    'Which of the following statements about osmotic and blood pressure is wrong?\nA. Osmotic pressure forces molecules out of the blood\nB. They are opposing each other\nC. Blood pressure varies a lot compared to osmotic pressure',
-    'A',
-  ],
-  [
-    'Which of the following types of cells does not contain nuclei?\nA. Stem cells\nB. RBCs\nC. Leukocytes',
-    'B',
-  ],
-  [
-    'Where is the location for old RBCs to be destroyed?\nA. Heart and tissues\nB. Bone marrow \nC. Liver and spleen',
-    'C',
-  ],
-  [
-    'Which type of cells has to do with leukemia?\nA. White blood cells\nB. Red blood cells\nC. Killer T cells',
-    'B',
-  ],
-  [
-    'Which kind of antigens do people with type O+ blood have?\nA. No antigen\nB. Antigen O\nC. Antigen Rh',
-    'C',
-  ],
-  [
-    'Which of the following blood vessels supply oxygenated blood to the heart?\nA. Coronary arteries\nB. Coronary veins\nC. Aorta',
-    'A',
-  ],
-  [
-    'Which of the following blood vessels transport deoxygenated blood to the lungs?\nA. Pulmonary arteries\nB. Pulmonary veins\nC. Aorta',
-    'A',
-  ],
-  [
-    'Where is the heart rate center located?\nA. Pituitary glands\nB. Medulla Oblongata\nC. Hypothalamus',
-    'B',
-  ],
-  [
-    "What is the parasympathetic system's effect on the heart rate?\nA. There is no relation\nB. It slows down the rate\nC. It speeds up the rate",
-    'B',
-  ],
-  [
-    'Which of the following factors is not leading to hypertension?\nA. Smoking\nB. Excessive salt intake\nC. Excessive water intake',
-    'C',
-  ],
-  [
-    'In which situation, the blood pressure reading will be higher?\nA. After amputation\nB. During systole\nC. During diastole',
-    'B',
-  ],
-  [
-    'Which of the following is a reasonable number for the volume of blood in our body?\nA. 5.6 L\nB. 3.0 L\nC. 12.4 L',
-    'A',
-  ],
-  [
-    'What are the tiny branch of capillaries in the intestines called?\nA. Lacteal\nB. Microtubules\nC. Villi',
-    'C',
-  ],
-];
-var questionList = questionSource;
+var questionList = [];
+getQuestions();
 
 export const TicTacToe = {
   setup: () => ({ cells: Array(9).fill(null) }),
@@ -210,7 +90,7 @@ function newQuestion() {
   var index = getRandomInt(questionList.length);
   if (questionList.length < 1) {
     alert('Out of problems! Reseting...');
-    questionList = questionSource;
+    getQuestions();
     return false;
   }
   var answer = prompt(
@@ -234,4 +114,12 @@ function newQuestion() {
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
+}
+
+function getQuestions() {
+  $('body').loading();
+  $.getScript('/questions.js').done(function (script) {
+    questionList = questionsJS;
+    $('body').loading('stop');
+  });
 }
